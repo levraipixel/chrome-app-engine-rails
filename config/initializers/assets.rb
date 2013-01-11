976348755248dffb3407670de9df80913533f33a
Yann Hourdel <yann.hourdel@gmail.com>
2013-01-11 18:06:08 +0100

    added initializer for distant assets

---
+++ config/initializers/assets.rb
@@ -0,0 +1,28 @@
+module Sass
+  module Rails
+    module Helpers
+
+      [:image, :video, :audio, :javascript, :stylesheet, :font].each do |asset_class|
+        class_eval %Q{
+          def #{asset_class}_url_with_host(asset)
+            builder = ::ChromeAppEngineRails::Builder.new(::Rails.application)
+            Sass::Script::String.new("url('http://" + builder.config.host + resolver.#{asset_class}_path(asset.value) + "')")
+          end
+        }, __FILE__, __LINE__ - 4
+      end
+      
+      class_eval %Q{
+        def image_chrome_url(asset)
+          Sass::Script::String.new("url('chrome-extension://__MSG_@@extension_id__/img/" + asset.value.to_s + "')")
+          end
+      }, __FILE__, __LINE__ - 3
+
+      class_eval %Q{
+        def font_chrome_url(asset)
+          Sass::Script::String.new("url('chrome-extension://__MSG_@@extension_id__/font/" + asset.value.to_s + "')")
+          end
+      }, __FILE__, __LINE__ - 3
+
+    end
+  end
+end
